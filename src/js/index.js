fa36e662fb7ccded3085cfec738715e4a3537b66
Alex Vydryakov <nt-vydryakov@mail.ru>
2024-07-17 14:38:09 +0300

    перед matchmedia

--- src/js/index.js
+++ src/js/index.js
@@ -11,6 +11,7 @@ const changer = function () {
   if (document.documentElement.clientWidth < 767.98) {
     console.log('меньше 767')
 
+    //запускаем свайпер
     const swiper = new Swiper('.swiper', {
       // Optional parameters
       modules: [Navigation, Pagination],
@@ -36,26 +37,140 @@ const changer = function () {
         invert: true
       }
     })
+    // меню скрыто 320p
+    document.querySelector('.menu').classList.add('hide')
+
+    // кнопки
+    let buttonOpenMenu = document.querySelector('.icon__menu')
+    let buttonCloseMenu = document.querySelector('.menu__close-menu')
+
+    let buttonOpenCall = document.querySelector('.icon__tel')
+    let buttonCloseCall = document.querySelector('.modal__call .modal__close')
+
+    let buttonOpenFeedback = document.querySelector('.icon__message')
+    let buttonCloseFeedback = document.querySelector('.modal__feedback .modal__close')
+
+    
+
+    // селекторы
+    let menu = document.querySelector('.menu')
+    let content = document.querySelector('.content')
+    let call = document.querySelector('.modal__call')
+    let feedback = document.querySelector('.modal__feedback')
+
+
+    // функции
+    let showMenu = function () {
+      menu.classList.remove('hide')
+    }
+    let hideMenu = function () {
+      menu.classList.add('hide')
+    }
+
+    let showСontent = function () {
+      content.classList.remove('hide')
+    }
+    let hideСontent = function () {
+      content.classList.add('hide')
+    }
+
+    let showСall = function () {
+      call.classList.remove('hide')
+    }
+    let hideСall = function () {
+      call.classList.add('hide')
+    }
+
+    let showFeedback = function () {
+      feedback.classList.remove('hide')
+    }
+    let hideFeedback = function () {
+      feedback.classList.add('hide')
+    }
+
+    window.addEventListener('click', function (evt) {
+      //открываем меню
+      if (evt.target === buttonOpenMenu) {
+        showMenu()
+
+        hideСontent()
+      }
+
+      //закрываем меню
+      if (evt.target === buttonCloseMenu) {
+        hideMenu()
+
+        showСontent()
+      }
+
+      //открываем call
+      if (evt.target === buttonOpenCall) {
+        // показываем call
+        showСall()
+
+        // скрываем меню
+        hideMenu()
+      }
+
+      // закрываем call
+      if (evt.target === buttonCloseCall) {
+        // скрываем call
+        hideСall()
+        // показываем меню
+        showMenu()
+      }
+
+      // показываем feedback
+      if (evt.target === buttonOpenFeedback) {
+        showFeedback()
+        hideMenu()
+      }
+
+      // скрываем feedback
+      if (evt.target === buttonCloseFeedback) {
+        hideFeedback()
+        showMenu()
+      }
+
+    })
   } else {
     console.log('больше 767')
-    let buttonOpen = document.querySelector('.footer__open')
-    let buttonClose = document.querySelector('.footer__close')
-    let swiper = document.querySelector('.swiper-wrapper')
 
-    buttonOpen.addEventListener('click', function () {
-      swiper.classList.remove('swiper-wrapper--height')
-      buttonOpen.classList.add('hide')
-      buttonClose.classList.remove('hide')
-    })
+    let buttonOpenCloseSwiper = document.querySelectorAll('.line__open-close')
+    let buttonOpenSwiper = document.querySelectorAll('.line__open-close--open')
+    let buttonCloseSwiper = document.querySelectorAll(
+      '.line__open-close--close'
+    )
+    let swiper = document.querySelectorAll('.swiper-wrapper')
 
-    buttonClose.addEventListener('click', function () {
-      swiper.classList.add('swiper-wrapper--height')
-      buttonOpen.classList.remove('hide')
-      buttonClose.classList.add('hide')
-    })
+    //показываем-скрываем свайперы
+    for (let i = 0; i < buttonOpenCloseSwiper.length; i++) {
+      buttonOpenCloseSwiper[i].addEventListener('click', function () {
+        swiper[i].classList.toggle('swiper-wrapper--height')
+        buttonOpenSwiper[i].classList.toggle('hide')
+        buttonCloseSwiper[i].classList.toggle('hide')
+      })
+    }
+
+    //показываем свайперы
+    // for (let i = 0; i < buttonOpenSwiper.length; i++) {
+    //   buttonOpenSwiper[i].addEventListener('click', function () {
+    //     swiper[i].classList.remove('swiper-wrapper--height')
+    //     buttonOpenSwiper[i].classList.add('hide')
+    //     buttonCloseSwiper[i].classList.remove('hide')
+    //   })
+    // }
+
+    // //скрываем свайперы
+    // for (let i = 0; i < buttonCloseSwiper.length; i++) {
+    //   buttonCloseSwiper[i].addEventListener('click', function () {
+    //     swiper[i].classList.add('swiper-wrapper--height')
+    //     buttonOpenSwiper[i].classList.remove('hide')
+    //     buttonCloseSwiper[i].classList.add('hide')
+    //   })
+    // }
   }
 }
-
 changer()
 
 import '../scss/style.scss'
